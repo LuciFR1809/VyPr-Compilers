922cffd74041d43460dfe1c23a458a08d2ce9928
Kumar Pranjal <60619224+kpranjal2047@users.noreply.github.com>
2021-04-13 16:57:01 +0530

    Minor bug fixes

--- Assignment2/lexer.py
+++ Assignment2/lexer.py
@@ -6,29 +6,29 @@
 # Abhishek Bapna - 2018A7PS0184H
 # Ashish Verma - 2018A7PS0009H
 
-#black=\033[30m
-#red=\033[31m
-#green=\033[32m
-#orange=\033[33m
-#blue=\033[34m
-#purple=\033[35m
-#cyan=\033[36m
-#lightgrey=\033[37m
-#darkgrey=\033[90m
-#lightred=\033[91m
-#lightgreen=\033[92m
-#yellow=\033[93m
-#lightblue=\033[94m
-#pink=\033[95m
-#lightcyan=\033[96m
-#BOLD = \033[1m
-#FAINT = \033[2m
-#ITALIC = \033[3m
-#UNDERLINE = \033[4m
-#BLINK = \033[5m
-#NEGATIVE = \033[7m
-#CROSSED = \033[9m
-#END = \033[0m
+# black=\033[30m
+# red=\033[31m
+# green=\033[32m
+# orange=\033[33m
+# blue=\033[34m
+# purple=\033[35m
+# cyan=\033[36m
+# lightgrey=\033[37m
+# darkgrey=\033[90m
+# lightred=\033[91m
+# lightgreen=\033[92m
+# yellow=\033[93m
+# lightblue=\033[94m
+# pink=\033[95m
+# lightcyan=\033[96m
+# BOLD = \033[1m
+# FAINT = \033[2m
+# ITALIC = \033[3m
+# UNDERLINE = \033[4m
+# BLINK = \033[5m
+# NEGATIVE = \033[7m
+# CROSSED = \033[9m
+# END = \033[0m
 
 # Code starts here
 import sys
@@ -36,184 +36,190 @@ import sys
 
 # Storing tokens in lists
 keywords = ['int', 'float', 'do', 'void', 'boolean', 'string', 'for', 'if', 'else',
-			'while', 'break', 'continue', 'switch', 'default', 'return', 'case', 'import', 'true', 'false']
+            'while', 'break', 'continue', 'switch', 'default', 'return', 'case', 'import', 'true', 'false']
 
 operators = ['+=', '-=', '*=', '/=', '%=', '==', '<=', '>=', '!=', '<<',
-			 '>>', '&&', '||', '+', '-', '*', '%', '/', '=', '!', '<', '>']
+                         '>>', '&&', '||', '+', '-', '*', '%', '/', '=', '!', '<', '>']
 
 delimiters = [';', ',', '(', ')', '{', '}', '\n', '\r', '\t', '\\']
 
 string_term = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-			   'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 legal_term = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-			  'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+              'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 numeric_term = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 sign_term = ['+', '-']
 
 
-def DFA(check_str, prev_op):
-	'''
-	Internal function used by lexeme() function to find a token
-	'''
-	matches = []
-	pairs = []
+def DFA(check_str, prev_op, prev_lex):
+    '''
+    Internal function used by lexeme() function to find a token
+    '''
+    matches = []
+    pairs = []
 
-	# check for end of string
-	if check_str[0] == "$" and len(check_str) == 1:
-		return None, None, None
+    # check for end of string
+    if check_str[0] == "$" and len(check_str) == 1:
+        return None, None, None
 
-	# check for blank space
-	if check_str[0] == " ":
-		return check_str[1:], None, None
+    # check for blank space
+    if check_str[0] == " ":
+        return check_str[1:], None, None
 
-	# check for keyword
-	key_match = [check_str.startswith(x) for x in keywords]
-	if any(key_match):
-		key = keywords[key_match.index(True)]
-		matches.append(len(key))
-		pairs.append(['keyword', key])
+    # check for keyword
+    key_match = [check_str.startswith(x) for x in keywords]
+    if any(key_match):
+        key = keywords[key_match.index(True)]
+        matches.append(len(key))
+        pairs.append(['keyword', key])
 
-	# check for operators
-	key_match = [check_str.startswith(x) for x in operators]
-	if any(key_match):
-		key = operators[key_match.index(True)]
-		matches.append(len(key))
-		if key in sign_term and prev_op == 'operator':
-			pairs.append(['sign'+key, key])
-		else:
-			pairs.append(['operator', key])
+    # check for operators
+    key_match = [check_str.startswith(x) for x in operators]
+    if any(key_match):
+        key = operators[key_match.index(True)]
+        matches.append(len(key))
+        if key in sign_term and (prev_op == 'operator' or prev_op == 'keyword' or prev_lex == '{' or prev_lex == '('):
+            pairs.append(['sign'+key, key])
+        else:
+            pairs.append(['operator', key])
 
-	# check for delimiters
-	key_match = [check_str.startswith(x) for x in delimiters]
-	if any(key_match):
-		key = delimiters[key_match.index(True)]
-		matches.append(len(key))
-		pairs.append(['delimiter', key])
+    # check for delimiters
+    key_match = [check_str.startswith(x) for x in delimiters]
+    if any(key_match):
+        key = delimiters[key_match.index(True)]
+        matches.append(len(key))
+        pairs.append(['delimiter', key])
 
-	# check for identifier
-	if check_str[0] in string_term:
-		for index, letter in enumerate(check_str):
-			if letter not in legal_term:
-				matches.append(index)
-				pairs.append(['identifier', check_str[:index]])
-				break
+    # check for identifier
+    if check_str[0] in string_term:
+        for index, letter in enumerate(check_str):
+            if letter not in legal_term:
+                matches.append(index)
+                pairs.append(['identifier', check_str[:index]])
+                break
 
-	# check for string literal
-	if check_str[0] == '"':
-		for index, letter in enumerate(check_str[1:]):
-			if letter == '"' and check_str[index] != '\\':
-				matches.append(index+2)
-				pairs.append(['string literal', check_str[:index+2]])
-				break
+    # check for string literal
+    if check_str[0] == '"':
+        for index, letter in enumerate(check_str[1:]):
+            if letter == '"' and check_str[index] != '\\':
+                matches.append(index+2)
+                pairs.append(['string literal', check_str[:index+2]])
+                break
 
-	# check for numerical literal
-	if check_str[0] in numeric_term or check_str[0] in sign_term:
-		isFloat = False
-		for index, letter in enumerate(check_str[1:]):
-			if letter not in numeric_term:
-				if letter == '.' and not isFloat:
-					isFloat = True
-				else:
-					if prev_op == 'sign+' or prev_op == 'sign-':
-						matches.append(index+2)
-						orig_str = check_str[:index+1]
-						if isFloat:
-							pairs.append(
-								['floating_literal', prev_op[-1] + orig_str])
-							break
-						else:
-							pairs.append(
-								['integer_literal', prev_op[-1] + orig_str])
-							break
-					else:
-						matches.append(index+1)
-						if isFloat:
-							pairs.append(
-								['floating_literal', check_str[:index+1]])
-							break
-						else:
-							pairs.append(
-								['integer_literal', check_str[:index+1]])
-							break
+    # check for numerical literal
+    if check_str[0] in numeric_term or check_str[0] in sign_term:
+        isFloat = False
+        for index, letter in enumerate(check_str[1:]):
+            if letter not in numeric_term:
+                if letter == '.' and not isFloat:
+                    isFloat = True
+                else:
+                    if prev_op == 'sign+' or prev_op == 'sign-':
+                        matches.append(index+2)
+                        orig_str = check_str[:index+1]
+                        if isFloat:
+                            pairs.append(
+                                ['floating_literal', prev_op[-1] + orig_str])
+                            break
+                        else:
+                            pairs.append(
+                                ['integer_literal', prev_op[-1] + orig_str])
+                            break
+                    else:
+                        matches.append(index+1)
+                        if isFloat:
+                            pairs.append(
+                                ['floating_literal', check_str[:index+1]])
+                            break
+                        else:
+                            pairs.append(
+                                ['integer_literal', check_str[:index+1]])
+                            break
 
-	# check for comment
-	if check_str[0] == '#':
-		matches.append(len(check_str))
-		pairs.append([None, None])
+    # check for comment
+    if check_str[0] == '#':
+        matches.append(len(check_str))
+        pairs.append([None, None])
 
-	# now find the token for longest matching lexeme
-	# final is final index in matches which should be used for token lexeme pairs
-	if(len(matches)) == 0:
-		if len(check_str) > 1:
-			return check_str[1:], 'invalid', check_str[0]
-		else:
-			return None, 'invalid', check_str[0]
+    # now find the token for longest matching lexeme
+    # final is final index in matches which should be used for token lexeme pairs
+    if(len(matches)) == 0:
+        if len(check_str) > 1:
+            return check_str[1:], 'invalid', check_str[0]
+        else:
+            return None, 'invalid', check_str[0]
 
-	final = matches.index(max(matches))
+    final = matches.index(max(matches))
 
-	# SPECIAL CASE - sign operators
-	if (prev_op != 'operator' or prev_op != 'keyword') and check_str[0] in sign_term:
-		final = 0
+    # SPECIAL CASE - sign operators
+    if (prev_op != 'operator' or prev_op != 'keyword' or prev_lex != '{' or prev_lex != '(') and check_str[0] in sign_term:
+        final = 0
 
-	end_pos = int(matches[final])
-	if end_pos < len(check_str):
-		return check_str[end_pos:], pairs[final][0], pairs[final][1]
-	elif end_pos == len(check_str):
-		return None, pairs[final][0], pairs[final][1]
-	else:
-		print("\033[31msome error in parsing\033[0m")
+    end_pos = int(matches[final])
+    if end_pos < len(check_str):
+        if (prev_op == 'sign+' or prev_op == 'sign-') and (pairs[final][0] == 'floating_literal' or pairs[final][0] == 'integer_literal'):
+            end_pos -= 1
+        return check_str[end_pos:], pairs[final][0], pairs[final][1]
+    elif end_pos == len(check_str):
+        return None, pairs[final][0], pairs[final][1]
+    else:
+        print("\033[31msome error in parsing\033[0m")
 
 
 def lexeme(string):
-	'''
-	Accepts a string and returns a generator object containing tokens
-	which can be accessed using a for loop or by using next() function
-	'''
-	lines = string.splitlines()
-	for line_number, line in enumerate(lines):
-		line = line.strip() + '$'
-		prev_op = None
-		if len(line) > 0:
-			while(line != None):
-				line, key, lexeme = DFA(line, prev_op)
-				if key is not None:
-					prev_op = key
-				if key != None and key != 'invalid' and key != 'sign+' and key != 'sign-':
-					yield {'line': line_number+1, 'token': key, 'lexeme': lexeme}
-				elif key == 'invalid':
-					yield {'line': line_number+1, 'token': 'invalid', 'lexeme': lexeme}
+    '''
+    Accepts a string and returns a generator object containing tokens
+    which can be accessed using a for loop or by using next() function
+    '''
+    lines = string.splitlines()
+    for line_number, line in enumerate(lines):
+        line = line.strip() + '$'
+        prev_op = None
+        prev_lex = None
+        if len(line) > 0:
+            while(line != None):
+                line, key, lexeme = DFA(line, prev_op, prev_lex)
+                if key is not None and key != 'invalid':
+                    prev_op = key
+                    prev_lex = lexeme
+                if key is not None and key != 'invalid' and key != 'sign+' and key != 'sign-':
+                    yield {'line': line_number+1, 'token': key, 'lexeme': lexeme}
+                elif key == 'invalid':
+                    yield {'line': line_number+1, 'token': 'invalid', 'lexeme': lexeme}
 
 
 # Driver Code
 def lexer(fname=None):
-	if fname is None:
-		filename = sys.argv[1]
-	else:
-		filename = fname
-	outfile = open(f'{filename.split(".")[0]}_output.txt', 'w', encoding='utf8')
-	parsefile = open(f'{filename.split(".")[0]}_output_parse.txt', 'w', encoding='utf8')
-	with open(filename, 'r', encoding='utf8') as f:
-		filestr = f.read()
-		for lex in lexeme(filestr):
-			if lex['token'] == 'invalid':
-				print(
-					f"!!! Error in line {lex['line']} while parsing {lex['lexeme']}")
-				print(
-					f"!!! Error in line {lex['line']} while parsing {lex['lexeme']}", file=outfile)
-				print(
-					f"{lex['line']} <!> Error <!> {lex['lexeme']}", file=parsefile)
-				
-			else:
-				print(
-					f"Line: {lex['line']}, Token: {lex['token']}, Lexeme: {lex['lexeme']}")
-				print(
-					f"Line: {lex['line']}, Token: {lex['token']}, Lexeme: {lex['lexeme']}", file=outfile)
-				print(
-					f"{lex['line']} <!> {lex['token']} <!> {lex['lexeme']}", file = parsefile)
+    if fname is None:
+        filename = sys.argv[1]
+    else:
+        filename = fname
+    outfile = open(
+        f'{filename.split(".")[0]}_output.txt', 'w', encoding='utf8')
+    parsefile = open(
+        f'{filename.split(".")[0]}_output_parse.txt', 'w', encoding='utf8')
+    with open(filename, 'r', encoding='utf8') as f:
+        filestr = f.read()
+        for lex in lexeme(filestr):
+            if lex['token'] == 'invalid':
+                print(
+                    f"!!! Error in line {lex['line']} while parsing {lex['lexeme']}")
+                print(
+                    f"!!! Error in line {lex['line']} while parsing {lex['lexeme']}", file=outfile)
+                print(
+                    f"{lex['line']} <!> Error <!> {lex['lexeme']}", file=parsefile)
+
+            else:
+                print(
+                    f"Line: {lex['line']}, Token: {lex['token']}, Lexeme: {lex['lexeme']}")
+                print(
+                    f"Line: {lex['line']}, Token: {lex['token']}, Lexeme: {lex['lexeme']}", file=outfile)
+                print(
+                    f"{lex['line']} <!> {lex['token']} <!> {lex['lexeme']}", file=parsefile)
 
 
 if __name__ == '__main__':
-	lexer()
+    lexer()
--- Assignment2/parse.py
+++ Assignment2/parse.py
@@ -6,30 +6,30 @@
 import sys
 import json
 
+
 class Tree(object):
     def __init__(self, data):
         self.data = data
         self.children = []
         self.depth = 0
-    
+
     def addnode(self, child):
         self.children.append(child)
         if self.depth <= child.depth:
             self.depth = child.depth+1
 
+
 def parse_lookup(parse_table, token, number):
-    if(parse_table[number].get(token, 'Error') != 'Error'):
-        return parse_table[number][token]
-    else:
-        return 'Error'
+    return parse_table[number].get(token, 'Error')
+
 
 def reduce_lookup(reduce_table, number):
     rule = reduce_table[number]
     lines = rule.split('->')
     return lines[0].strip(), lines[1].strip()
-    
 
-def parse(fname = None):
+
+def parse(fname=None):
     if fname is None:
         filename = sys.argv[1]
     else:
@@ -40,25 +40,31 @@ def parse(fname = None):
 
     with open("Goto Table.json", 'r', encoding='utf8') as f2:
         goto_table = json.load(f2)
-    
-    with open("Rules.json", 'r', encoding = 'utf8') as f3:
+
+    with open("Rules.json", 'r', encoding='utf8') as f3:
         reduce_table = json.load(f3)
 
     with open(filename, 'r', encoding='utf8') as f:
         lines = f.readlines()
+        last = lines[-1].split('<!>')
+        last = [x.strip() for x in last]
+        if last != []:
+            lines.append(f'{last[0]} <!> stop <!> $')
+        else:
+            lines.append('0 <!> stop <!> $')
         stack = ['0']
         tree_stack = []
         Err_stack = []
         count = 0
-        #get parse_table, reduce_table and goto_table
+        # get parse_table, reduce_table and goto_table
         for _ in lines:
             lin_num, token, lexeme = lines[count].split('<!>')
             token = token.strip()
             lexeme = lexeme.strip('\n')
             lexeme = lexeme.strip()
             print(f'{count} : {stack}')
-    
-            #bring token in required format as in parse table
+
+            # bring token in required format as in parse table
 
             if token == 'integer_literal':
                 token = 'INT_LIT'
@@ -66,23 +72,25 @@ def parse(fname = None):
                 token = 'FLT_LIT'
             if token == 'identifier':
                 token = 'ID'
-            if token  == 'string_literal':
+            if token == 'string_literal':
                 token = 'STR_LIT'
             if token == 'operator':
                 token = lexeme
-            if token  == 'delimiter':
+            if token == 'delimiter':
                 token = lexeme
             if token == 'keyword':
                 token = lexeme
             if token == 'Error':
-                print(f'\033[0;31mLexical Error:: Line number {lin_num}, UnIdentified character {lexeme} found\033[0m')
-                Err_stack.append(f'Lexical Error:: Line number {lin_num}, UnIdentified character {lexeme} found')
+                print(
+                    f'\033[0;31mLexical Error:: Line number {lin_num}, UnIdentified character {lexeme} found\033[0m')
+                Err_stack.append(
+                    f'Lexical Error:: Line number {lin_num}, UnIdentified character {lexeme} found')
                 continue
 
-            #look up parse table for next character 
-            next_op = parse_lookup(parse_table, token, stack[len(stack)-1])
+            # look up parse table for next character
+            next_op = parse_lookup(parse_table, token, stack[-1])
 
-            #shift action
+            # shift action
             if next_op[0] == 's':
                 stack.append(token)
                 tree_stack.append(Tree(token))
@@ -90,53 +98,52 @@ def parse(fname = None):
                 count += 1
                 continue
 
-            #reduce action
+            # reduce action
             elif next_op[0] == 'r':
-                non_terminal, condense = reduce_lookup(reduce_table, next_op[1:])
+                non_terminal, condense = reduce_lookup(
+                    reduce_table, next_op[1:])
                 parent = Tree(non_terminal)
                 condense = condense.split()
                 condense.reverse()
                 # to pop out elements from the stack
                 flag = True
                 condense = [i.strip() for i in condense if i]
-            
+
                 if condense[0] != 'ϵ':
                     for i in condense:
                         if flag:
                             stack.pop()
-                        if stack[len(stack)-1] == i:
+                        if stack[-1] == i:
                             parent.addnode(tree_stack.pop())
                             stack.pop()
                             flag = True
                         else:
                             flag = False
-                
+
                 # now look up goto table and find the next rule
-                new_num = parse_lookup(goto_table, non_terminal, stack[len(stack) - 1])
+                new_num = parse_lookup(
+                    goto_table, non_terminal, stack[-1])
 
                 if new_num == 'Error':
-                    print(f'\033[0;31mSyntax Error:: Line number {lin_num}, Wrong Syntactic Input {lexeme}, Please check again\033[0m')
-                    Err_stack.append(f'Syntax Error:: Line number {lin_num}, Wrong Syntactic Input {lexeme}, Please check again')
+                    print(
+                        f'\033[0;31mSyntax Error:: Line number {lin_num}, Wrong Syntactic Input {lexeme}, Please check again\033[0m')
+                    Err_stack.append(
+                        f'Syntax Error:: Line number {lin_num}, Wrong Syntactic Input {lexeme}, Please check again')
                 else:
                     stack.append(non_terminal)
                     tree_stack.append(parent)
                     stack.append(new_num)
 
-            else :
-                print(f'\033[0;31mSyntax Error:: Line number {lin_num}, Wrong Syntactic Input {lexeme}, Please check again\033[0m')
-                Err_stack.append(f'Syntax Error:: Line number {lin_num}, Wrong Syntactic Input {lexeme}, Please check again')
+            else:
+                print(
+                    f'\033[0;31mSyntax Error:: Line number {lin_num}, Wrong Syntactic Input {lexeme}, Please check again\033[0m')
+                Err_stack.append(
+                    f'Syntax Error:: Line number {lin_num}, Wrong Syntactic Input {lexeme}, Please check again')
                 prev_lin_num = lin_num
                 while prev_lin_num == lin_num:
                     count += 1
                     lin_num, token, lexeme = lines[count].split('<!>')
-            
-            
-
-
-
-    
-
 
 
 if __name__ == '__main__':
-	parse()
+    parse()
